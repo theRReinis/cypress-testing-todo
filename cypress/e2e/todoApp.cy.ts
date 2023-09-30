96f376006acbf7521e9b9dcc8351c615a94450f2
Reinis Romanovs <r93romanovs@gmail.com>
2023-09-30 16:56:51 +0300

    Use cy custom commands for common funtions

--- cypress/e2e/todoApp.cy.ts
+++ cypress/e2e/todoApp.cy.ts
@@ -1,49 +1,34 @@
-describe("Add new tasks into ToDo app", () => {
-
+describe("Add new tasks into Todo app", () => {
   it("User can add a task to the list by typing into input field and pressing enter", () => {
     cy.visit("");
-    cy.get('[data-testid="main-input"]').type("Cook dinner{enter}");
-    cy.get('[data-testid="list-item"]')
-      .should("have.length", 1)
-      .should('have.text', 'Cook dinner');
+    cy.addNewTask("Cook dinner");
+    cy.assertTaskListSize(1).should("have.text", "Cook dinner");
   });
 
   it("User added task appears to at the end of the list", () => {
     cy.visit("");
-    cy.get('[data-testid="main-input"]').type("Cook dinner{enter}");
-    cy.get('[data-testid="main-input"]').type("Read book{enter}");
-    cy.get('[data-testid="list-item"]')
-      .should("have.length", 2).last().should('have.text', 'Read book');
+    cy.addNewTask("Cook dinner");
+    cy.addNewTask("Read book");
+    cy.assertTaskListSize(2).last().should("have.text", "Read book");
   });
 });
 
-describe("Complete tasks into ToDo app", () => {
-    
-    it("User can mark the task as completed by clicking on the check button", () => {
-      cy.visit("");
-      cy.get('[data-testid="main-input"]').type("Cook dinner{enter}");
-      cy.get('[data-testid="list-item"]')
-        .should("have.length", 1).find('[type=checkbox]').click({force:true});
+describe("Complete tasks into Todo app", () => {
+  it("User can mark the task as completed by clicking on the check button", () => {
+    cy.visit("");
+    cy.addNewTask("Cook dinner");
+    cy.assertTaskListSize(1).find("[type=checkbox]").click({ force: true });
 
-      cy.reload();
-      cy.get('[data-testid="list-item"]').within((el) => {
-        cy.wrap(el).find('[type=checkbox]').should('have.attr', 'checked')
-        cy.wrap(el).find('label').should('have.css', 'text-decoration', 'line-through solid rgb(225, 219, 218)');
-      });
+    cy.reload();
+    cy.assertTaskIsMarkedAsCompleted();
+  });
 
-    });
+  it("User can mark the task as completed by clicking text label", () => {
+    cy.visit("");
+    cy.addNewTask("Cook dinner");
+    cy.assertTaskListSize(1).find("label").click();
 
-    it("User can mark the task as completed by clicking text label", () => {
-        cy.visit("");
-        cy.get('[data-testid="main-input"]').type("Cook dinner{enter}");
-        cy.get('[data-testid="list-item"]')
-          .should("have.length", 1).find('label').click();
-  
-        cy.reload();
-        cy.get('[data-testid="list-item"]').within((el) => {
-          cy.wrap(el).find('[type=checkbox]').should('have.attr', 'checked')
-          cy.wrap(el).find('label').should('have.css', 'text-decoration', 'line-through solid rgb(225, 219, 218)');
-        });
-  
-      });
+    cy.reload();
+    cy.assertTaskIsMarkedAsCompleted();
+  });
 });
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,37 +1,60 @@
 /// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+
+Cypress.Commands.add("addNewTask", addNewTask);
+Cypress.Commands.add("assertTaskListSize", assertTaskListSize);
+Cypress.Commands.add(
+  "assertTaskIsMarkedAsCompleted",
+  assertTaskIsMarkedAsCompleted
+);
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Cypress {
+    interface Chainable {
+      /**
+       * Add new task by typing text into input field and pressing Enter
+       * @param taskText
+       */
+      addNewTask(taskText: string): void;
+      /**
+       * Assert task list size have expected number of tasks
+       * @param expectedNumberOfTasks
+       * @return Task list
+       *
+       */
+      assertTaskListSize(expectedNumberOfTasks: number): Cypress.Chainable;
+      /**
+       * Assert the task is marked as completed by verifying 2 things:
+       *
+       *    Checkbox have attribute: checked
+       *    Text label have CSS style: line-through
+       */
+      assertTaskIsMarkedAsCompleted(): void;
+    }
+  }
+}
+
+export function addNewTask(taskText: string): void {
+  cy.get('[data-testid="main-input"]').type(`${taskText}{enter}`);
+}
+
+export function assertTaskListSize(
+  expextedTaskAmount: number
+): Cypress.Chainable {
+  return cy
+    .get('[data-testid="list-item"]')
+    .should("have.length", expextedTaskAmount);
+}
+
+export function assertTaskIsMarkedAsCompleted(): void {
+  cy.assertTaskListSize(1).within((el) => {
+    cy.wrap(el).find("[type=checkbox]").should("have.attr", "checked");
+    cy.wrap(el)
+      .find("label")
+      .should(
+        "have.css",
+        "text-decoration",
+        "line-through solid rgb(225, 219, 218)"
+      );
+  });
+}
