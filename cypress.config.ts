41fb80f4768c067664c1a20eac7a107d65cc8eb4
Reinis Romanovs <r93romanovs@gmail.com>
2023-09-30 18:41:51 +0300

    Add test reports

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     baseUrl: "https://antondedyaev.github.io/todo_list/",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
